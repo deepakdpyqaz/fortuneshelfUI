882ebf7f6212b314b8759f17f7bb7a415766a3e4
deepak <deepakdpyqaz@gmail.com>
2022-08-05 15:33:59 +0530

    fixing bugs

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import dotenv from "dotenv";
 dotenv.config();
 const configs={
-    backendUrl: process.env.REACT_APP_backendUrl||"https://fortuneshelfserver.herokuapp.com/"
+    backendUrl: "https://fortuneshelfserver.herokuapp.com/"
 }
 
 export default configs;
